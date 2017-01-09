1c1456bce7b8ca6c2409b46d6dd11787ea12cd90
arekbee <arekbee@MacBook-Pro-arekbee.local>
2017-01-09 22:06:03 +0100

    Big changes in websites

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -45,3 +45,21 @@ $(document).ready(function() {
 $('#city-video').mediaelementplayer(/* Options */);
 
 //var __player = new MediaElementPlayer('#city-video');
+
+
+// fancebox 
+$('.fancybox-thumbs').fancybox({
+				prevEffect : 'none',
+				nextEffect : 'none',
+
+				closeBtn  : false,
+				arrows    : false,
+				nextClick : true,
+      
+				helpers : {
+					thumbs : {
+						width  : 50,
+						height : 50
+					}
+				}
+			});
